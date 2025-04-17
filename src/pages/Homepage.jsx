5197196c39e3d9eda9a07ab903454b684669f6aa
manirul <manirulshumon@gmail.com>
2025-04-16 19:50:21 -0600

    initial content on components and edit app

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Blog from '../components/Blog'
+import Footer from '../components/Footer'
+
+const Homepage = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Blog />
+      <Footer />
+
+    
+    </>
+  )
+}
+
+export default Homepage
